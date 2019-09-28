01aea880da95bc43a905c3ccd293f13417ba52d5
Maykelmattar <55925674+Maykelmattar@users.noreply.github.com>
2019-09-29 01:43:43 +0300

    Update PhoneCallTrap.java

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -1,74 +1,86 @@
 package io.tcg.phonecalltrap;
 
-import android.content.BroadcastReceiver;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 import android.content.Context;
-import android.content.Intent;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
-import java.lang.reflect.Method;
-import com.android.internal.telephony.ITelephony;
+
+import org.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 public class PhoneCallTrap extends CordovaPlugin {
 
-public class IncomingCallReceiver extends BroadcastReceiver {
+    CallStateListener listener;
+
     @Override
-    public void onReceive(Context context, Intent intent) {
-        String msg = "";
-        String number= " ";
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        prepareListener();
 
-        ITelephony telephonyService;
-        try {
-            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-         number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        listener.setCallbackContext(callbackContext);
 
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)){
-                TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-                try {
-                    Method m = tm.getClass().getDeclaredMethod("getITelephony");
+        return true;
+    }
 
-                    m.setAccessible(true);
-                    telephonyService = (ITelephony) m.invoke(tm);
-
-                    if ((number != null)) {
-                        telephonyService.endCall();
-                        //Toast.makeText(context, "Ending the call from: " + number, Toast.LENGTH_SHORT).show();
-                    }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                msg = "RINGING";
-                // Toast.makeText(context, "Ring " + number, Toast.LENGTH_SHORT).show();
-
-            }
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)){
-                msg = "OFFHOOK";
-                //   Toast.makeText(context, "Answered " + number, Toast.LENGTH_SHORT).show();
-            }
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)){
-                msg = "IDLE";
-                //  Toast.makeText(context, "Idle "+ number, Toast.LENGTH_SHORT).show();
-                JSONObject jso = new JSONObject();
-                String outpool = "";
-                //JSONArray msgJSON = new JSONArray();
-              //  String number="muky";
-          //   String number=intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                try{
-                    jso.put("state", msg);
-                    jso.put("incomingNumber", number);
-                    outpool = jso.toString();
-                }catch(JSONException e){
-                    outpool = "{state: '" + msg + "', incomingNumber: '" + number + "'}";
-                }
-        
-                PluginResult result = new PluginResult(PluginResult.Status.OK, outpool);
-                result.setKeepCallback(true);
-        
-                callbackContext.sendPluginResult(result);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void prepareListener() {
+        if (listener == null) {
+            listener = new CallStateListener();
+            TelephonyManager TelephonyMgr = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+            TelephonyMgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
         }
     }
 }
+
+class CallStateListener extends PhoneStateListener {
+
+    private CallbackContext callbackContext;
+
+    public void setCallbackContext(CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
+
+    public void onCallStateChanged(int state, String incomingNumber) {
+       super.onCallStateChanged(state, incomingNumber);
+
+        if (callbackContext == null) return;
+
+        String msg = "";
+String number= " ";
+        switch (state) {
+            case TelephonyManager.CALL_STATE_IDLE:
+            msg = "IDLE";
+            break;
+
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+            msg = "OFFHOOK";
+            break;
+
+            case TelephonyManager.CALL_STATE_RINGING:
+                number = incomingNumber;
+            msg = "RINGING";
+
+                break;
+        }
+
+        JSONObject jso = new JSONObject();
+        String outpool = "";
+        //JSONArray msgJSON = new JSONArray();
+      //  String number="muky";
+  //   String number=intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        try{
+            jso.put("state", msg);
+            jso.put("incomingNumber", number);
+            outpool = jso.toString();
+        }catch(JSONException e){
+            outpool = "{state: '" + msg + "', incomingNumber: '" + number + "'}";
+        }
+
+        PluginResult result = new PluginResult(PluginResult.Status.OK, outpool);
+        result.setKeepCallback(true);
+
+        callbackContext.sendPluginResult(result);
+    }
 }
